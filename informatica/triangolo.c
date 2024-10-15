f537914c12b9cc557c02e6b8a59a966d3a6d33db
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2024-10-15 11:19:04 +0000

    b

--- informatica/triangolo.c
+++ informatica/triangolo.c
@@ -2,14 +2,14 @@
 (perché un triangolo possa essere tale la somma di ogni coppia lati deve essere maggiore dell’altro)*/
 #include <stdio.h>
 int main(){
-float a=0,b=0,c=0;
-printf("Inserisci la lunghezza dei tre lati");
-scanf("%f%f%f",&a,&b,&c);
-    if(a+b>c && a+c>b && b+c>a)
-        printf("è un triangolo");
-    else
-        printf("Non è un triangolo");
-    
+    float a=0,b=0,c=0;
+    printf("Inserisci la lunghezza dei tre lati");
+    scanf("%f%f%f",&a,&b,&c);
+        if(a+b>c && a+c>b && b+c>a)
+            printf("è un triangolo");
+        else
+            printf("Non è un triangolo");
+        
 
 
 }
