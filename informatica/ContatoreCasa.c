a72edd79f166044f57a606dd446568ee86022ee5
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2024-10-29 17:07:01 +0000

    grtg

---
+++ informatica/ContatoreCasa.c
@@ -0,0 +1,21 @@
+/*Scrivi un programma in C che faccia inserire dei numeri interi all'utente: 
+il programma si deve fermare quando la somma dei numeri inseriti raggiunge 100
+oppure quando sono stati inseriti 5 numeri.
+Alla fine mostrare il numero di valori inseriti dall'utente (usare contatore).*/
+
+#include <stdio.h>
+int main(){
+    int num=0, somma=0, contatore=1;
+    printf("Inserisci un numero: ");
+    scanf("%a", &num);
+    somma=num;
+        if(somma>100){
+            printf("Hai superato la soglia")
+        }
+        else
+            printf("Inserisci un altro numero");
+            
+
+
+
+}
