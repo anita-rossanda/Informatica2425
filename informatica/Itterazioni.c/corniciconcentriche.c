7e6955e97204e66d9b9ad7ea18fa523802b155ae
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2024-12-02 10:54:54 +0000

    h

---
+++ informatica/Itterazioni.c/corniciconcentriche.c
@@ -0,0 +1,6 @@
+/*Scrivi un programma che disegna sullo schermo 3 cornici quadrate
+concentriche costituite dal carattere “*” rispettivamente di lato 12, 8, e 4*/
+#include <stdio.h>
+int main(){
+    int num1, num2, i, somma=0;
+}
