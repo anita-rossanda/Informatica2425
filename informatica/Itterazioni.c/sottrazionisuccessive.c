29163e1138b7047be142e4afe0c4161842a8418e
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2024-12-05 09:27:41 +0000

    tgtg

---
+++ informatica/Itterazioni.c/sottrazionisuccessive.c
@@ -0,0 +1,18 @@
+/*calcolare il quoz tra due numeri applicando il metodo
+delle sottrazioni successive*/
+
+#include <stdio.h>
+int main(){
+    int num1, num2, i, sottrazione=0;
+    do{
+        printf("Inserisci il primo valore");
+        scanf("%d",&num1);
+    }while(num1<=0);
+
+    do{
+        printf("Inserisci il primo valore");
+        scanf("%d",&num2);
+    }while(num2<=0);
+    
+}
+    
