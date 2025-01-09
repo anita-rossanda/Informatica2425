416971178d4e6c8a94fea72cf81c9ac25df0215d
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2025-01-09 10:51:00 +0000

    jj

---
+++ informatica/pg247n11.c
@@ -0,0 +1,18 @@
+/*scrivi un programma che, dati in input n numeri
+interi e un numero x, determini:
+-quanti numeri sono maggiori di x;
+-quanti numeri sono minori di x;
+-quanti numeri sono uguali a x;*/
+
+#include <stdio.h>
+int main(){
+    int x, n,cnt=0;
+
+    printf("insertisci un numero x:");
+    scanf("%d",&x);
+    printf("insertisci un numero n:");
+    scanf("%d",&n);
+    
+
+    
+}
