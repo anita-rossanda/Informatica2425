c981b1ef6535d5e4001fd529435cdcac922ff0de
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2025-01-13 10:55:18 +0000

    f

--- informatica/mediavoti.c
+++ informatica/mediavoti.c
@@ -4,5 +4,9 @@ di un numero se i voti da inserire sono terminati accettando come
 risposta S oppure N*/
 #include <stdio.h>
 int main(){
-    int num;
+    int voto;
+    do{
+        printf("inserisci il voto: ");
+        scanf("%d", &voto);
+    }while(voto<0)
 }
