461c1b6e02da161eed4c8f75551b9a21524c1391
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2025-01-23 10:45:52 +0000

    ddd

--- informatica/rossanda_es3_230125.c
+++ informatica/rossanda_es3_230125.c
@@ -2,19 +2,18 @@
 somma è multiplo di 3 di 5 oppure di 7.*/
 #include <stdio.h>
 int main(){
-    int n,num_q,cifra,somma=0;
+    int n,num_q,cifra,somma=0, cnt=0;
     do{
         printf("Inserisci un numero: ");
         scanf("%d",&n);
         num_q=n;
         cifra=num_q%10;
         num_q=num_q/10;
+        cnt++;
         somma+=cifra;
         printf("la somma vale: ");
         scanf("%d",&somma);
     }while(n<=0);
-
-        
 
     if(somma%3==0){
         printf("è multiplo di 3: ");
@@ -28,6 +27,4 @@ int main(){
         printf("è multiplo di 7: ");
         scanf("%d",&somma);
     }
-    
-    
 }
