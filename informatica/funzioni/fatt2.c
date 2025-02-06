684747dba0d4ab82e575c8832063bb926bb5348e
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2025-02-06 10:10:10 +0000

    GGTGHT

--- informatica/funzioni/fatt2.c
+++ informatica/funzioni/fatt2.c
@@ -2,15 +2,17 @@
 long fattoriale(int _num);
 int main(){
     int num=0;
+    long fattoriale;
     do{
         printf("inserisci un numero: ");
         scanf("%d", &num);
     }while(num<=0);
+    fattoriale=_fattoriale(num);
 }
 long fattoriale(int _num){
     long sumfat=1;
     for(int i=1; i<=_num; i++){
        sumfat*=i;
     }
-    printf("il fatt è: ",sumfat);
+    return sumfat;
 }
