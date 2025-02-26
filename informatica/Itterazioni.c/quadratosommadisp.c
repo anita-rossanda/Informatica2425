1a6228fb7b4eebcd84a338ddd4c655b6af8b99be
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2025-02-26 08:54:34 +0000

    r

---
+++ informatica/Itterazioni.c/quadratosommadisp.c
@@ -0,0 +1,15 @@
+/*N=5--->N2=1+3+5+7+8=25*/
+#include <stdio.h>
+void quadrato(int *_n, int *_n2);
+int main(){
+    int n, n2;
+    do{
+        printf("Inserisci num di cui vuoi trovare il quadrato: ");
+        scanf("%d",&n);
+    }while(n<=0);
+
+    
+}
+void quadrato(int *_n, int *_n2){
+
+}
