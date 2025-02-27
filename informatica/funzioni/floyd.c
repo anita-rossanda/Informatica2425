5579f7f04b94bdcd7053b1fb7800f30c86cbad79
anita-rossanda <anita.rossanda@galileiostiglia.edu.it>
2025-02-27 09:31:31 +0000

    5

---
+++ informatica/funzioni/floyd.c
@@ -0,0 +1,32 @@
+/*sviluppare un programma in C
+ che stampi il triangolo di floyd ES:
+N=5
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15*/
+
+#include <stdio.h>
+void TriangoloFloyd (int *_num);
+
+int main(){
+    int n;
+
+    do{
+        printf("inserisci un valore: ");
+        scanf("%d",&n);
+
+    }while
+    TriangoloFloyd (&n);
+
+}
+void TriangoloFloyd (int *_num){
+
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;J++){
+            
+
+        }
+    }
+}
